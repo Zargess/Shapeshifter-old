47470f0d023511d5be54392530b76149098cfffc
Zargess <haagh9@hotmail.com>
2014-04-06 23:38:35 +0200

    sedfsfd

--- Shapeshifter/Shapeshifter.Desktop/Functionality/Helpers/SettingsHelper.cs
+++ Shapeshifter/Shapeshifter.Desktop/Functionality/Helpers/SettingsHelper.cs
@@ -7,13 +7,10 @@ using System.Text;
 using System.Windows;
 using Microsoft.Win32;
 
-namespace Shapeshifter.Desktop.Functionality.Helpers
-{
-    public static class SettingsHelper
-    {
+namespace Shapeshifter.Desktop.Functionality.Helpers {
+    public static class SettingsHelper {
 
-        public static void ResetDefaults()
-        {
+        public static void ResetDefaults() {
             var applicationName = Assembly.GetExecutingAssembly().GetName().Name;
 
             var currentUser = Registry.CurrentUser;
@@ -23,18 +20,15 @@ namespace Shapeshifter.Desktop.Functionality.Helpers
             Debug.Assert(softwareKey != null, "softwareKey != null");
 
             var flamefusionKey = softwareKey.OpenSubKey("Flamefusion", RegistryKeyPermissionCheck.ReadWriteSubTree);
-            if (flamefusionKey != null)
-            {
+            if (flamefusionKey != null) {
 
                 var onlyThisProgramPresent = flamefusionKey.GetSubKeyNames().Length == 1 && flamefusionKey.GetValueNames().Length == 0;
 
                 var applicationKey = flamefusionKey.OpenSubKey(applicationName, RegistryKeyPermissionCheck.ReadWriteSubTree);
-                if (applicationKey != null)
-                {
+                if (applicationKey != null) {
                     flamefusionKey.DeleteSubKeyTree(applicationName, false);
 
-                    if (onlyThisProgramPresent)
-                    {
+                    if (onlyThisProgramPresent) {
                         softwareKey.DeleteSubKeyTree("Flamefusion", false);
                     }
                 }
@@ -46,8 +40,7 @@ namespace Shapeshifter.Desktop.Functionality.Helpers
             softwareKey.Dispose();
         }
 
-        public static void SaveSetting(string key, string value)
-        {
+        public static void SaveSetting(string key, string value) {
             var applicationName = Assembly.GetExecutingAssembly().GetName().Name;
 
             var currentUser = Registry.CurrentUser;
@@ -57,25 +50,21 @@ namespace Shapeshifter.Desktop.Functionality.Helpers
             Debug.Assert(softwareKey != null, "softwareKey != null");
 
             var flamefusionKey = softwareKey.OpenSubKey("Flamefusion", RegistryKeyPermissionCheck.ReadWriteSubTree);
-            if (flamefusionKey == null)
-            {
+            if (flamefusionKey == null) {
                 flamefusionKey = softwareKey.CreateSubKey("Flamefusion", RegistryKeyPermissionCheck.ReadWriteSubTree);
             }
 
             softwareKey.Dispose();
 
-            if (flamefusionKey != null)
-            {
+            if (flamefusionKey != null) {
                 var applicationKey = flamefusionKey.OpenSubKey(applicationName, RegistryKeyPermissionCheck.ReadWriteSubTree);
-                if (applicationKey == null)
-                {
+                if (applicationKey == null) {
                     applicationKey = flamefusionKey.CreateSubKey(applicationName, RegistryKeyPermissionCheck.ReadWriteSubTree);
                 }
 
                 flamefusionKey.Dispose();
 
-                if (applicationKey != null)
-                {
+                if (applicationKey != null) {
                     applicationKey.SetValue(key, value, RegistryValueKind.String);
 
                     applicationKey.Dispose();
@@ -83,8 +72,7 @@ namespace Shapeshifter.Desktop.Functionality.Helpers
             }
         }
 
-        public static string GetSetting(string key, string defaultValue = "")
-        {
+        public static string GetSetting(string key, string defaultValue = "") {
             var applicationName = Assembly.GetExecutingAssembly().GetName().Name;
 
             var currentUser = Registry.CurrentUser;
@@ -94,34 +82,29 @@ namespace Shapeshifter.Desktop.Functionality.Helpers
             Debug.Assert(softwareKey != null, "softwareKey != null");
 
             var flamefusionKey = softwareKey.OpenSubKey("Flamefusion", RegistryKeyPermissionCheck.ReadWriteSubTree);
-            if (flamefusionKey == null)
-            {
+            if (flamefusionKey == null) {
                 flamefusionKey = softwareKey.CreateSubKey("Flamefusion", RegistryKeyPermissionCheck.ReadWriteSubTree);
             }
 
             softwareKey.Dispose();
 
-            if (flamefusionKey != null)
-            {
+            if (flamefusionKey != null) {
                 var applicationKey = flamefusionKey.OpenSubKey(applicationName, RegistryKeyPermissionCheck.ReadWriteSubTree);
-                if (applicationKey == null)
-                {
+                if (applicationKey == null) {
                     applicationKey = flamefusionKey.CreateSubKey(applicationName, RegistryKeyPermissionCheck.ReadWriteSubTree);
                 }
 
                 string value = defaultValue;
-                if (applicationKey != null)
-                {
+                if (applicationKey != null) {
                     value = (string)applicationKey.GetValue(key, defaultValue);
 
                     applicationKey.Dispose();
                 }
 
-                if(value == defaultValue)
-                {
-                    
+                if (value == defaultValue) {
+
                     //the key is not present for the application. check for global Flamefusion settings instead.
-                    value = (string) flamefusionKey.GetValue(key, defaultValue);
+                    value = (string)flamefusionKey.GetValue(key, defaultValue);
 
                 }
 
